ebffa6056373766ff1dba0c7714fbc8c41cec21a
Andrew Yu <andrewyu209@gmail.com>
2022-02-27 00:16:14 -0500

    Added Authethication and mutations

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { authMiddleware } = require('./utils/auth');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 // import our typeDefs and resolvers
@@ -12,7 +13,8 @@ const startServer = async() => {
     // create a new Apollo server and pass in our schema data
     const server = new ApolloServer({
         typeDefs,
-        resolvers
+        resolvers,
+        context: authMiddleware
     });
     // Start the Apollo server
     await server.start();
